691540d3813a1054fee4b677f69772e12ea9696b
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-10-03 11:18:50 +0200

    Iteration 4 solved from start to vikingAttack method

--- src/viking.js
+++ src/viking.js
@@ -48,4 +48,63 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+    constructor () {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+        this.randomSaxon = this.pickRandomSoldier(this.saxonArmy)
+        this.randomViking = this.pickRandomSoldier(this.vikingArmy)
+    }
+
+    addViking (viking) {
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon (saxon) {
+        this.saxonArmy.push(saxon);
+    }
+    
+    pickRandomSoldier (army) {
+        let randomIndex = 0;
+        randomIndex = Math.floor(Math.random() * army.length);
+        return randomIndex;
+    }
+
+    vikingAttack () {
+        const currentSaxon = this.saxonArmy[this.randomSaxon];
+        const currentViking = this.vikingArmy[this.randomViking];
+        currentSaxon.receiveDamage(currentViking.strength);
+        const currentSaxonIndex = this.saxonArmy.indexOf(currentSaxon);
+        if (currentSaxon.health <= 0) {
+            this.saxonArmy.splice(currentSaxonIndex, 1);
+            return `A Saxon has died in combat`
+        }
+        return `A Saxon has received ${currentViking.strength} points of damage`
+    }
+}
+
+
+/* const newArmy = ["fulanito", "menganito", "pepito"];
+
+let viking, saxon, war;
+
+function generateViking() {
+    const name = 'Harald';
+    const strength = 150;
+    const health = 300;
+    return new Viking(name, health, strength);
+}
+
+function generateSaxon() {
+    const health = 60;
+    const strength = 25;
+    return new Saxon(health, strength);
+}
+
+beforeEach(() => {
+    viking = generateViking();
+    saxon = generateSaxon();
+    war = new War();
+});
+
+console.log(pickRandomSoldier(newArmy)); */
