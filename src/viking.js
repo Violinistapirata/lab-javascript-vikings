aebf3b6169976db2bf2121f717b4b1e5d4a80343
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-10-03 11:35:15 +0200

    Iteration 4 solved

--- src/viking.js
+++ src/viking.js
@@ -93,6 +93,16 @@ class War {
         }
         return `${currentViking.name} has received ${currentSaxon.strength} points of damage`
     }
+
+    showStatus () {
+        if (this.saxonArmy.length === 0) {
+            return "Vikings have won the war of the century!"
+        } else if (this.vikingArmy.length === 0) {
+            return "Saxons have fought for their lives and survived another day..."
+        } else {
+            return "Vikings and Saxons are still in the thick of battle."
+        }
+    }
 }
 
 
