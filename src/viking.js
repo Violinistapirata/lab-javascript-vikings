b8caed395a481201a898753ddd9e8536a3370935
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-10-03 09:27:32 +0200

    Iteration 1 solved

--- src/viking.js
+++ src/viking.js
@@ -1,7 +1,16 @@
 // Soldier
 class Soldier {
     constructor(health, strength) {
-        
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(damage){
+        this.health -= damage;
     }
 }
 
